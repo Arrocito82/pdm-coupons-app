ca51c2e5a89c7caa0a103870ade8ce91795b4257
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-06-06 10:32:35 -0600

    se cambio el textview prueba por el nombre de usuario en home

--- CouponsApp/app/src/main/java/com/example/couponsapp/InicioActivity.java
+++ CouponsApp/app/src/main/java/com/example/couponsapp/InicioActivity.java
@@ -92,8 +92,7 @@ public class InicioActivity extends AppCompatActivity implements NavigationView.
             String userString = cuenta.getEmail().substring(0, indexUser);
 
             if(usuarioControl.userExist(userString, cuenta.getEmail()) == 0){
-
-                usuarioControl.insertUsuario(new Usuario(
+                usuario=new Usuario(
                         0, //no tiene relevancia para insert
                         3,
                         0,
@@ -105,7 +104,8 @@ public class InicioActivity extends AppCompatActivity implements NavigationView.
                         "",
                         1,
                         ""
-                ));
+                );
+                usuarioControl.insertUsuario(usuario);
 
                 //Enviar correo electronico de bienvenida
                 String mEmail = cuenta.getEmail();
@@ -154,7 +154,11 @@ public class InicioActivity extends AppCompatActivity implements NavigationView.
         }
 
         //Inicializar en la opcion home
-        getSupportFragmentManager().beginTransaction().add(R.id.content, new HomeFragment()).commit();
+        Bundle data = new Bundle();
+        data.putString("username",usuario.getUsername());
+        Fragment home=new HomeFragment();
+        home.setArguments(data);
+        getSupportFragmentManager().beginTransaction().add(R.id.content,home ).commit();
         setTitle("Home");
         setSupportActionBar(toolbar);
 
